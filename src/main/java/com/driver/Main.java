ffa01c42498e5264ee61fcd9bccdc8effc867f7a
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-02-07 20:43:59 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,8 @@
 package com.driver;
 
 public class Main {
-  
+    RWOnly obj = new RWOnly();
+    // if setter is not created error is :'name' has private access in 'RWOnly'
+    obj.setName("Nikhil Rawat");
+    System.out.println(obj.getName());
 }
